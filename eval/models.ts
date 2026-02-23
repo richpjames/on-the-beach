1b064d02ccf8182f021caee4d518e8ad2601626c
Rich James <rich@genericgreeting.co.uk>
2026-02-23 20:17:38 +0100

    update models

--- eval/models.ts
+++ eval/models.ts
@@ -1,9 +1,1 @@
-export const VISION_MODELS = [
-  "mistral-small-3-2-25-06",
-  "mistral-medium-3-1-25-08",
-  "mistral-large-3-25-12",
-  "ministral-3-14b-25-12",
-  "ministral-3-8b-25-12",
-  "ministral-3-3b-25-12",
-  "pixtral-large-2411",
-];
+export const VISION_MODELS = ["mistral-ocr-latest", "mistral-small-2506", "mistral-medium-2508"];
